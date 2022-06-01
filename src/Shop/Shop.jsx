744f7d7602bc4268a088d7b88966f3b1ab8679fa
JakubWesoly <kubawesoly2006@gmail.com>
2022-06-01 03:14:21 +0200

    Dodano koszyk

--- src/Shop/Shop.jsx
+++ src/Shop/Shop.jsx
@@ -1,8 +1,15 @@
 import React, { useEffect, useState } from 'react';
+import { BsCart4 } from 'react-icons/bs';
+import { AiOutlineClose } from 'react-icons/ai';
+import Modal from '../Modal/Modal';
 import ShopItem from './ShopItem/ShopItem';
 
 const Shop = () => {
   const [items, setItems] = useState([]);
+  const [cartItems, setCartItems] = useState(
+    JSON.parse(localStorage.getItem('cart')) || []
+  );
+  const [isCartShown, setIsCartShown] = useState(false);
 
   useEffect(() => {
     fetch('http://localhost:3001/products')
@@ -11,16 +18,82 @@ const Shop = () => {
         setItems(data);
       });
   }, []);
+
+  // useEffect(() => {
+  //   setCartItems(
+  //     cartItems.map((item, index) => {
+  //       return { id: index, ...item };
+  //     })
+  //   );
+  // }, []);
+
+  // useEffect(() => {
+  //   setCartItems(JSON.parse(localStorage.getItem('cart')));
+  // }, [localStorage.getItem('cart')]);
+
   return (
     <>
       <header className='section-header'>
-        <h2>Sklep </h2>
+        <h2>Sklep</h2>
       </header>
       <div className='section-shop'>
         {items.map(
-          (item) => item.quantity > 0 && <ShopItem item={item} key={item.id} />
+          (item) =>
+            item.quantity > 0 && (
+              <ShopItem item={item} update={setCartItems} key={item.id} />
+            )
         )}
       </div>
+      <div className='section-shop-cart' onClick={() => setIsCartShown(true)}>
+        <BsCart4 style={{ marginTop: '12px' }} />
+      </div>
+      <Modal show={isCartShown} changeVisibility={setIsCartShown}>
+        <div className='section-shop-cart-modal'>
+          <h2>Koszyk</h2>
+          <ul className='options-container cart-container'>
+            {cartItems.map((item, index) => {
+              return (
+                <li className='options-item cart-item'>
+                  <p>{item.productName}</p>
+                  <p>Ilość: {item.productQuantity}szt.</p>
+                  <p>Cena: {item.productPrice}zł</p>
+                  <AiOutlineClose
+                    className='cart-close'
+                    onClick={() => {
+                      setCartItems(
+                        cartItems.filter(
+                          (it) => it.productName !== item.productName
+                        )
+                      );
+                      localStorage.removeItem('cart');
+                      localStorage.setItem(
+                        'cart',
+                        JSON.stringify(
+                          cartItems.filter(
+                            (it) => it.productName !== item.productName
+                          )
+                        )
+                      );
+                    }}
+                  />
+                </li>
+              );
+            })}
+          </ul>
+          <div className='cart-summary'>
+            <button className='button-small'>Złóż zamówienie</button>
+            <p style={{ justifySelf: 'right' }}>
+              Łączny koszt: &nbsp;
+              {cartItems.reduce(
+                (total, currentValue) =>
+                  (total = total + currentValue.productPrice),
+                0
+              )}
+              zł
+            </p>
+          </div>
+        </div>
+      </Modal>
     </>
   );
 };
--- src/Shop/ShopItem/ShopItem.jsx
+++ src/Shop/ShopItem/ShopItem.jsx
@@ -2,17 +2,61 @@ import React, { useEffect, useState } from 'react';
 import Modal from '../../Modal/Modal';
 import NumberSelector from '../../NumberSelector/NumberSelector';
 
-const ShopItem = ({ item }) => {
+const ShopItem = ({ item, update }) => {
   const [isModalShown, setIsModalShown] = useState(false);
   const [size, setSize] = useState(1);
-  const [seller, setSeller] = useState('');
+  const [seller, setSeller] = useState([]);
 
   useEffect(() => {
     fetch(`http://localhost:3001/users?id=${item.sellerId}`)
       .then((res) => res.json())
-      .then((data) => setSeller(data[0].login))
+      .then((data) => setSeller(data[0]))
       .catch((err) => console.error(err));
   }, [item.sellerId]);
+
+  const handleAddingProduct = () => {
+    const cart = JSON.parse(localStorage.getItem('cart'));
+
+    if (cart == null) {
+      localStorage.setItem(
+        'cart',
+        JSON.stringify([
+          {
+            productId: item.id,
+            productName: item.name,
+            productQuantity: size,
+            productPrice: item.price * size,
+            sellerId: seller.id,
+            buyerId: JSON.parse(localStorage.getItem('userInfo'))[0],
+          },
+        ])
+      );
+    } else {
+      let hasFound = false;
+      for (let i = 0; i < cart.length; i++) {
+        if (cart[i].productId === item.id) {
+          cart[i].productPrice += parseInt(item.price) * parseInt(size);
+          cart[i].productQuantity =
+            parseInt(cart[i].productQuantity) + parseInt(size);
+          hasFound = true;
+          break;
+        }
+      }
+      if (!hasFound) {
+        cart.push({
+          productId: item.id,
+          productName: item.name,
+          productQuantity: size,
+          productPrice: item.price * size,
+          sellerId: seller.id,
+          buyerId: JSON.parse(localStorage.getItem('userInfo'))[0],
+        });
+      }
+      localStorage.setItem('cart', JSON.stringify(cart));
+      update(JSON.parse(localStorage.getItem('cart')));
+    }
+    alert('dodano do koszyka');
+  };
 
   return (
     <div className='section-shop-item' key={item.id}>
@@ -28,8 +72,8 @@ const ShopItem = ({ item }) => {
         <div className='section-shop-modal'>
           <img src={item.imageUrl} alt='kukurydza' srcset='' />
           <h3>{item.name}</h3>
-          <p>Sprzedawca: {seller}</p>
-          <p>{item.desc}</p>
+          <p>Sprzedawca: {seller.login}</p>
+          <pre>{item.desc}</pre>
           <p>
             {item.quantity} szt. | {item.price}zł
           </p>
@@ -40,7 +84,14 @@ const ShopItem = ({ item }) => {
             max={item.quantity}
           />
           <p>Cena produktu: {item.price * size}zł</p>
-          <button className='button-small'>Dodaj do koszyka</button>
+          <button
+            className='button-small'
+            onClick={() => {
+              handleAddingProduct();
+            }}
+          >
+            Dodaj do koszyka
+          </button>
         </div>
       </Modal>
     </div>
